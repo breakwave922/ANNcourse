db0ef6324ef536a2823fb730848c601c56b186ce
breakwave922 <ffvxb@mail.missouri.edu>
2019-03-02 21:40:18 -0600

    add part 2 updated

--- proj1_2.py
+++ proj1_2.py
@@ -4,9 +4,11 @@ import matplotlib.pyplot as plt
 import ipywidgets as widgets
 from IPython.display import display
 from IPython.display import clear_output
+from ipywidgets import IntProgress
 import sys
 import random
 import csv
+import time
 
 np.random.seed(2000)  # this is what I used to get your random numbers!!!
 
@@ -27,16 +29,22 @@ train3_fname='./Part2_dat/Part2_3_Train.csv'
 train1_dat = np.genfromtxt(train1_fname, delimiter=',')
 train3_dat = np.genfromtxt(train3_fname, delimiter=',')
 
+
 ###last column is the right value that img represents
 train1_dat=np.append(train1_dat,np.ones((train1_dat.shape[0],1)),axis=1)
 train3_dat=np.append(train3_dat,3*np.ones((train3_dat.shape[0],1)),axis=1)
 train13_dat=np.concatenate((train1_dat,train3_dat),axis=0)  ##merge two data
+
+
+
+
 
 #define several dimension para
 img_dim=28   ##image dim
 layerh_n=2   ## #of neurons in hidden layer
 layero_n=2
 #imagesc( reshape(Matrix1_Test(:,1),[28 28]) )
+#plt.imshow(train13_dat[0,0:-1].reshape(img_dim,img_dim))
 
 #initialize wgt
 nh_w = np.random.normal(0, 1, (img_dim,img_dim,layerh_n))   ##hidden layer feature maps-1&2
@@ -56,9 +64,11 @@ y=np.array([[1,0],[0,1]])  ##first/sec row corrsponds to class1&2
 ###############################################
 # Epochs
 ###############################################
-epoch = 2000  # how many epochs?
+epoch = 500 # how many epochs?
 err = np.zeros((epoch, 1))  # lets record error to plot (get a convergence plot)
 inds = np.arange(np.size(train13_dat,0))  # array of our training indices (data point index references)
+f = IntProgress(min=0, max=epoch)  # instantiate the bar (you can "see" how long alg takes to run)
+display(f)  # display the bar!
 
 for k in range(epoch):
     # init error
@@ -120,3 +130,21 @@ plt.ylabel('Error')
 plt.xlabel('Epoch')
 plt.title('Convergence Plot')
 plt.show()
+
+# last, run through out data, what should label be, what did we get?
+inds = np.random.permutation(inds)
+for i in range(np.size(inds)):
+        # what index?
+    inx = inds[i]
+    # forward pass
+    v = np.ones(layerh_n+1,)  # last one is for bias
+    for j in range(layerh_n):
+        v[j] = np.multiply(train13_dat[inx,0:-1].reshape(img_dim,img_dim),nh_w[:,:,j]).sum()+b_h[j]
+        v[j] = acti(v[j])
+
+        #oo = np.array([np.dot(v.T, no_w),np.dot(v, no_w2)])  # output neuron 0&1 fires, taking hidden neuron 1 and 2 as input
+    oo = no_w @ v
+    o = acti(oo)  # result of output 0&1 !!!
+
+
+    print("Sample " + str(i) + ": label " + str(train13_dat[inx,-1]) + ": got " + str(o))
